77f13be36e88612c355f3f2981489a46d5364198
Aloes03 <102353332+Aloes03@users.noreply.github.com>
2022-03-25 10:01:18 +0100

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,12 @@
+#a program to determine the type of triangle
+
+side_a = float(input("enter the length of the first side: "))
+side_b = float(input("input the length of the second side: "))
+side_c = float(input("enter thr length of thr third side: ))
+
+if side_a == side_b == and side_b == side_c:
+	print("equilateral triangle")
+elif side_a == side_b or side_b == side_c or side_a == side_c:
+	print("isoceless triangle")
+else:
+	print("you are mad")
